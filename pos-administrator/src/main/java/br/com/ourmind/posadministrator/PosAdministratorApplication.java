d066fb0431a48b94c84c807e05e9c53cd053e7d6
Joao Santos <joaosantosdev@hotmail.com>
2021-03-06 16:00:32 -0300

    create project administrator

---
+++ pos-administrator/src/main/java/br/com/ourmind/posadministrator/PosAdministratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.ourmind.posadministrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosAdministratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosAdministratorApplication.class, args);
+	}
+
+}
---
+++ pos-administrator/src/test/java/br/com/ourmind/posadministrator/PosAdministratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ourmind.posadministrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosAdministratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
