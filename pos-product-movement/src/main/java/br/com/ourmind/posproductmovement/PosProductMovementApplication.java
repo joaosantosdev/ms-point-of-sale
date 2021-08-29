379914113b0f2b855cf8e3a41aedc61ed5ca2c33
Joao Santos <joaosantosdev@hotmail.com>
2021-08-29 18:38:58 -0300

    create project pos-product-movement

---
+++ pos-product-movement/src/main/java/br/com/ourmind/posproductmovement/PosProductMovementApplication.java
@@ -0,0 +1,13 @@
+package br.com.ourmind.posproductmovement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosProductMovementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosProductMovementApplication.class, args);
+	}
+
+}
