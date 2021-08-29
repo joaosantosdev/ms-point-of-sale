0f287970919cfba1ab48f3fbce4f68d9bbb735c3
Joao Santos <joaosantosdev@hotmail.com>
2021-08-29 18:17:03 -0300

    pos-administrator config eureka client

--- pos-administrator/src/main/java/br/com/ourmind/posadministrator/PosAdministratorApplication.java
+++ pos-administrator/src/main/java/br/com/ourmind/posadministrator/PosAdministratorApplication.java
@@ -1,23 +1,15 @@
 package br.com.ourmind.posadministrator;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class PosAdministratorApplication implements CommandLineRunner {
+public class PosAdministratorApplication {
 
-	@Value("${spring.datasource.password}")
-	private String pass;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(PosAdministratorApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(this.pass);
 	}
 
 }
