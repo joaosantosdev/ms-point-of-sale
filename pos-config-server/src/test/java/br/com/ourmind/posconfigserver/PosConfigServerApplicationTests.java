be1e5278f6f83d0969d68ba841f9242de0df477f
Joao Santos <joaosantosdev@hotmail.com>
2021-08-29 22:36:38 -0300

    create project config server

---
+++ pos-config-server/src/test/java/br/com/ourmind/posconfigserver/PosConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ourmind.posconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
