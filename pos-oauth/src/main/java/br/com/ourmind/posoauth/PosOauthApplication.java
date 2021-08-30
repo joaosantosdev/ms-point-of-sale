907657eaf86812bb4471f5de121884fd560377bd
Joao Santos <joaosantosdev@hotmail.com>
2021-08-29 23:09:24 -0300

    create project pos-oauth

---
+++ pos-oauth/src/main/java/br/com/ourmind/posoauth/PosOauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.ourmind.posoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosOauthApplication.class, args);
+	}
+
+}
---
+++ pos-oauth/src/test/java/br/com/ourmind/posoauth/PosOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ourmind.posoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
