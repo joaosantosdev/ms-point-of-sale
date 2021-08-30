ab2848aa863c9f57bf30a3bf27f29a6b435debb9
Joao Santos <joaosantosdev@hotmail.com>
2021-08-29 23:09:42 -0300

    create project pos-freebie-roulette

---
+++ pos-freebie-roulette/src/main/java/br/com/ourmind/posfreebieroulette/PosFreebieRouletteApplication.java
@@ -0,0 +1,13 @@
+package br.com.ourmind.posfreebieroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosFreebieRouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosFreebieRouletteApplication.class, args);
+	}
+
+}
